cdeee3668ad87c36b0791f304c7e7ca286278407
Ingolf Hill <66317712+Turtulla@users.noreply.github.com>
2020-06-05 12:53:38 +0200

    First running App

---
+++ werferstein.org.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DllRegister
+{
+    partial class werferstein_org : Form
+    {
+        Assembly asssembly;
+        public werferstein_org(Assembly ass)
+        {
+            asssembly = ass;
+            InitializeComponent();
+            //this.Text = String.Format("About {0}", AssemblyTitle);
+            this.Text = String.Format("About {0}", Program.ProgramName);
+
+            this.labelProductName.Text = Program.ProgramName;
+            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+            this.labelCopyright.Text = "Ingolf Hill Copyright ©  2020";//AssemblyCopyright;
+            this.labelCompanyName.Text = "werferstein.org";//AssemblyCompany;
+
+
+            //this.textBoxDescription.Text = AssemblyDescription;
+            this.textBoxDescription.Text = Program.ProgramDescription;
+        }
+
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                
+                object[] attributes = asssembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(asssembly.CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return asssembly.GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = asssembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = asssembly.GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = asssembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = asssembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void labelCompanyName_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"https://werferstein.org/");
+        }
+    }
+}
